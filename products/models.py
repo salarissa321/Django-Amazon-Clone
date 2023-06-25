6280889aa7868435a26d1d65c29e81afc07c4310
salarissa321 <salarissa321@gmail.com>
2023-06-25 17:47:01 +0200

    design products models pt 1

--- products/models.py
+++ products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model) :
+    name = models.CharField(max_length=120)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    description = models.TextField(max_length=30000)
+    subtitel = models.TextField(max_length=500)
+    sku = models.IntegerField()
+    brand = models.ForeignKey('Brand', related_name='product_brand', on_delete=models.SET_NUL, null= True , blank=True )
+
+    def__str__(self):
+       return self.name
+
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='brands')
+
+    def__str__(self):
+       return self.name
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, related_name='review_author' , on_delete=models.SET_NULL, null=True , blank=True)
+    product = models.ForeignKey(Product, related_name='product_review', on_delete=models.CASCADE)
+    review = models.TextField(max_length=500)
+    rate = models.IntegerField() 
+    create_date = models.DateTimeField(default=timezone.now)
+
+
+
